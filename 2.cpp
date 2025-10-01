2c5616bb03ab3cbfb586697446c8a7d3d1c305e8
namanarora2707 <namanarora270704@gmail.com>
2025-10-01 09:49:59 +0530

    Add Two Numbers

--- 2.cpp
+++ 2.cpp
@@ -35,10 +35,12 @@ int main() {
     ListNode* l1 = new ListNode(2);
     l1->next = new ListNode(4);
     l1->next->next = new ListNode(3);
+    l1->next->next->next = new ListNode(7);
 
     ListNode* l2 = new ListNode(5);
     l2->next = new ListNode(6);
     l2->next->next = new ListNode(4);
+    l2->next->next->next = new ListNode(9);
 
     ListNode* result = addTwoNumbers(l1, l2);
     while (result) {
