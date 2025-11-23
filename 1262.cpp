28eecc815d33012074892fd1295f109e3b45eacd
namanarora2707 <namanarora270704@gmail.com>
2025-11-23 07:37:49 +0530

    Greatest Sum Divisible by Three

---
+++ 1262.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxSumDivThree(vector<int>& nums) {
+    vector<int> dp(3, INT_MIN);
+    dp[0] = 0;
+    
+    for (int x : nums) {
+        vector<int> temp = dp;
+        for (int r = 0; r < 3; r++) {
+            int newR = (r + x % 3) % 3;
+            temp[newR] = max(temp[newR], dp[r] + x);
+        }
+        dp = temp;
+    }
+    
+    return dp[0];
+}
+int main() {
+    vector<int> nums={1,2,3,4,5};
+    cout<<maxSumDivThree(nums)<<endl;
+    return 0;
+}
